99a025ab4c7782715d23b6c7ba1b29d88a45111d
Aerdiansha <ahmaderdiansha.9a@gmail.com>
2025-02-05 02:47:44 +0700

    reset

--- template/header.php
+++ template/header.php
@@ -11,5 +11,5 @@
         <link href="<?= $main_url ?>assets/sb-admin/css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         
-        <link rel="shortcut icon" href="<?= $main_url ?>assets/images/toga.png" type="image/x-icon">
+        <link rel="shortcut icon" href="<?= $main_url ?>assets/image/toga.png" type="image/x-icon">
     </head>
